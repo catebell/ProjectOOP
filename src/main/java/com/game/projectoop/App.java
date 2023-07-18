f07f2e9fcb9764bb5286c5fc16699cfce68cce3e
Cate <cate10aprile@gmail.com>
2023-07-18 09:35:10 +0200

    Update

--- src/main/java/com/game/projectoop/App.java
+++ src/main/java/com/game/projectoop/App.java
@@ -5,6 +5,12 @@ import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.app.scene.LoadingScene;
 import com.almasb.fxgl.app.scene.SceneFactory;
+import com.almasb.fxgl.core.util.LazyValue;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.input.UserAction;
+import com.almasb.fxgl.input.virtual.VirtualButton;
+import javafx.scene.input.KeyCode;
+
 import static com.almasb.fxgl.dsl.FXGL.*;
 
 
@@ -12,6 +18,9 @@ public class App extends GameApplication {
     public enum EntityType{
         PLAYER,PLATFORM
     }
+
+    //? private LazyValue<LevelEndScene> levelEndScene = new LazyValue<>(() -> new LevelEndScene());
+    private Entity player;
 
     @Override
     protected void initSettings(GameSettings settings) {
@@ -29,6 +38,20 @@ public class App extends GameApplication {
         settings.setApplicationMode(ApplicationMode.DEVELOPER);
     }
 
+    @Override
+    protected void initInput() {
+        getInput().addAction(new UserAction("Left"){
+            @Override
+            protected void onAction() {
+                player.getComponent(PlayerComponent.class).left();
+            }
+
+            @Override
+            protected void onActionEnd() {
+                player.getComponent(PlayerComponent.class).stop();
+            }
+        }, KeyCode.A, VirtualButton.LEFT);
+    }
 
     @Override
     protected void initGame() {
