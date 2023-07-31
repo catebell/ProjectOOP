2da47b6c5cb456269b0545aec018ec074e7d4ae4
Cate <cate10aprile@gmail.com>
2023-07-31 15:41:58 +0200

    pods

---
+++ src/main/resources/assets/TiledStuff/cryo-pod-3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="cryo-pod-3" tilewidth="32" tileheight="32" tilecount="2" columns="1">
+ <image source="../levels/cryo-pod-3.png" trans="000000" width="59" height="87"/>
+</tileset>
